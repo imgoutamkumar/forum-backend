30402c74a9901a4f4403f95531a9181b30b74b3c
Goutam Kumar <goutamkumar42010no@gmail.com>
2026-03-30 16:39:33 +0530

    minor fix

--- src/controllers/replyController.js
+++ src/controllers/replyController.js
@@ -1,18 +1,18 @@
 import { createReplyService } from "../services/ReplyService";
 
- export const createReply=async(req, res) =>{
-    try {
-      const { commentId, content } = req.body;
-      const userId = req.userId; // assuming auth middleware
+export const createReply = async (req, res) => {
+  try {
+    const { commentId, content } = req.body;
+    const userId = req.userId; // assuming auth middleware
 
-      const reply = await createReplyService(
-        commentId,
-        userId,
-        content
-      );
+    const reply = await createReplyService(
+      commentId,
+      userId,
+      content
+    );
 
-      res.status(201).json(reply);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
+    res.status(201).json(reply);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
+}
